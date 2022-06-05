985a457340e997ceb04717c2e8837ba05c203273
Ali Emre <aliemrecaylak@gmail.com>
2022-06-05 21:50:32 +0300

    dokunmatik kontroller bitti

--- Assets/Scripts/Characters/shooter.cs
+++ Assets/Scripts/Characters/shooter.cs
@@ -7,14 +7,34 @@ public class shooter : MonoBehaviour
    
     [SerializeField] GameObject mermi;
     private Vector3 _vec;
+    private bool sorgu = false;
 
-    void FixedUpdate()
+    void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             _vec = new Vector3(transform.position.x, transform.position.y, transform.position.z + 1f);
             Instantiate(mermi, _vec, Quaternion.identity);
-            Debug.Log("Instantiated");
+            StartCoroutine("bekle");
         }
+
+        if (Input.touchCount == 1)
+        {
+            Touch parmak = Input.GetTouch(0);
+            if (parmak.deltaPosition.y < -32.0f && sorgu == false)
+            {
+                _vec = new Vector3(transform.position.x, transform.position.y, transform.position.z + 1f);
+                Instantiate(mermi, _vec, Quaternion.identity);
+                StartCoroutine("bekle");
+            }
+        }
+    }
+
+
+    IEnumerator bekle() //tekte birden fazla atmasını engelliyoruz.
+    {
+        sorgu = true;
+        yield return new WaitForSeconds(1);
+        sorgu = false;
     }
 }
